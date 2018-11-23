8db30c50f240062495900c82244cc165721045b8
= <=>
2018-11-22 22:00:51 -0200

    Ajustes na pagina principal e forma que as queries sao feitas

--- project/core/views.py
+++ project/core/views.py
@@ -3,20 +3,30 @@ from .forms import PaymentForm
 from .models import Payment
 from graphos.sources.model import ModelDataSource
 from graphos.renderers import highcharts
+from django.db.models import Sum
 import datetime
 
 today = datetime.date.today()
 now   = datetime.datetime.now()
 
 def home(request):
-	print(today)
-	queryset = Payment.objects.filter(initial_date__gte='2005-01-05', final_date__lte=today).order_by('initial_date')
+	initial_date = '{}-{}-{}'.format(today.year, today.month, '01')
+	final_data   = '{}-{}-{}'.format(today.year, today.month, '30')
+
+	payment_filter = Payment.objects.filter(initial_date__gte=initial_date, final_date__lte=final_data, is_renda=False)
+
+	total_value = payment_filter.aggregate(
+		Sum('value')
+	)
+	queryset = payment_filter.order_by('initial_date')
+
 	data_source =  ModelDataSource(queryset, fields=['initial_date','value'])
 	chart = highcharts.LineChart(data_source)
 
 	return render(request, 'core/home.html', {
-		'data_atual': now.month,
+		'data_atual': today.month,
 		'payments': queryset,
+		'total_value': total_value['value__sum'],
 		'line_chart': chart
 	})
 
